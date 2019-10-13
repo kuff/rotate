d2d4c68906d758e4e408ddace604ab64138b8c04
kuff <petergl@pm.me>
2019-10-13 16:48:11 +0200

    Add caching

--- rotated_image.py
+++ rotated_image.py
@@ -1,5 +1,6 @@
 import cv2
-import imutils
+import imutils  # should be phased out before final release!
+import numpy
 from utils import *
 
 
@@ -8,13 +9,16 @@ class RotatedImage:
         self.image_path = image_path
         self.image_path_name = get_file_name(self.image_path)
         self.rotation = rotation
+        self.rotated_image = None
         self.image = cv2.imread(image_path)
         if self.image is None:
             raise(IOError)
 
-    def _rotate(self, rotation):
+    def _rotate(self):
         # TODO: use own algorithm
-        return imutils.rotate_bound(self.image, rotation)
+        return imutils.rotate_bound(self.image, self.rotation)
 
     def show(self):
-        cv2.imshow(self.image_path_name, self._rotate(self.rotation))
+        if self.rotated_image is None:  # caching
+            self.rotated_image = self._rotate()
+        cv2.imshow(self.image_path_name, self.rotated_image)
